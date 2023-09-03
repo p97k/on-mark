2cf7fa8a9e9cf5ebb43d4d2e1f1e042d94fafa76
parham <parham.khoshbin@digikala.com>
2023-09-03 16:22:48 +0330

    feat: add base http methods

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"github.com/p97k/on-mark/products"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -20,13 +22,81 @@ func (p *Products) ServeHTTP(response http.ResponseWriter, request *http.Request
 		return
 	}
 
+	if request.Method == http.MethodPost {
+		p.addProducts(response, request)
+		return
+	}
+
+	if request.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		finalUrl := reg.FindAllStringSubmatch(request.URL.Path, -1)
+
+		if len(finalUrl) != 1 {
+			p.l.Println("invalid URI more than one id")
+			http.Error(response, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(finalUrl[0]) != 2 {
+			p.l.Println("invalid URI more than capture group id")
+			http.Error(response, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := finalUrl[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("invalid URI unable to convert to num", idString)
+			http.Error(response, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, response, request)
+		return
+	}
+
 	response.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(response http.ResponseWriter, request *http.Request) {
+	p.l.Println("handle GET product")
+
 	lp := products.GetProducts()
 	err := lp.ToJSON(response)
 	if err != nil {
 		http.Error(response, "unable to convert json!", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProducts(response http.ResponseWriter, request *http.Request) {
+	p.l.Println("handle POST product")
+
+	prod := &products.Product{}
+	err := prod.FromJSON(request.Body)
+	if err != nil {
+		http.Error(response, "Ops, unable to create json", http.StatusBadRequest)
+	}
+
+	products.AddProduct(prod)
+
+	p.l.Printf("Product: %#v", prod)
+}
+
+func (p *Products) updateProduct(id int, response http.ResponseWriter, request *http.Request) {
+	p.l.Println("handle PUT product")
+
+	prod := &products.Product{}
+	err := prod.FromJSON(request.Body)
+	if err != nil {
+		http.Error(response, "Ops, unable to create json", http.StatusBadRequest)
+	}
+
+	err = products.UpdateProduct(id, prod)
+	if err == products.ErrProductNotFound {
+		http.Error(response, "product not found", http.StatusNotFound)
+	}
+
+	if err != nil {
+		http.Error(response, "something wrong", http.StatusInternalServerError)
 	}
 }
--- products/products.go
+++ products/products.go
@@ -2,22 +2,28 @@ package products
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedAt   string
-	UpdatedAt   string
-	DeletedAt   string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedAt   string  `json:"-"`
+	UpdatedAt   string  `json:"-"`
+	DeletedAt   string  `json:"-"`
 }
 
 type Products []*Product
+
+func (p *Product) FromJSON(reader io.Reader) error {
+	e := json.NewDecoder(reader)
+	return e.Decode(p)
+}
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
@@ -26,6 +32,41 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = generateNextId()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
+}
+
+func generateNextId() int {
+	lp := productList[len(productList)-1]
+	lp.ID++
+	return lp.ID
 }
 
 var productList = []*Product{
@@ -39,7 +80,7 @@ var productList = []*Product{
 		UpdatedAt:   time.Now().UTC().String(),
 	},
 	&Product{
-		ID:          1,
+		ID:          2,
 		Name:        "Espresso",
 		Description: "Short and strong coffee without milk",
 		Price:       1.99,
